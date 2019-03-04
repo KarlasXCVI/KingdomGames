318c2f27a45be948cc38f279db6f7a9bb9fbf618
Kyle Gosling <43758608+kyleGosling@users.noreply.github.com>
2019-03-04 13:55:47 +0000

    Updated UI
    Updated the Start ,Main and Game UI.

---
+++ Assets/Scripts/GUI/AnyButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnyButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.anyKeyDown)
+        {
+            Invoke("LoadMainMenu", 5);
+        }
+		
+	}
+
+    void LoadMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
